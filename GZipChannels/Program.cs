d677c570f9c3aaff1efd2576c6581cb4642b5d65
Evgeny Shirobokov <EShi>
2021-01-30 15:55:04 +0300

    Add project files.

---
+++ GZipChannels/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GZipChannels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
