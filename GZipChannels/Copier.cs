9bd3a0d94a6064cb399515283c33dbc40065f097
Evgeny Shirobokov <EShi>
2021-01-30 18:37:52 +0300

    refactor

---
+++ GZipChannels/Copier.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace GZipChannels
+{
+    internal class Copier
+    {
+        private readonly string _source;
+        private readonly string _target;
+        private readonly Channel<byte[]> _channel;
+        private const int BlockSize = 10 * 1024 * 1024;
+        private const int ChannelSize = 100;
+
+        public Copier(string source, string target)
+        {
+            _source = source;
+            _target = target;
+            _channel = Channel.CreateBounded<byte[]>(ChannelSize);
+        }
+
+        public async Task Run()
+        {
+            var fileWriter = Task.Run(WriteTargetFile);
+
+            await ReadFileToChannel();
+
+            await fileWriter;
+        }
+
+        private async Task ReadFileToChannel()
+        {
+            await using var source = new FileStream(_source, FileMode.Open, FileAccess.Read);
+
+            var bytes = new byte[BlockSize];
+            int bytesRead;
+
+            do
+            {
+                bytesRead = await source.ReadAsync(bytes.AsMemory());
+
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                if (bytesRead == BlockSize)
+                {
+                    await _channel.Writer.WriteAsync(bytes);
+                }
+
+                if (bytesRead < BlockSize)
+                {
+                    var arr = new byte[bytesRead];
+                    Buffer.BlockCopy(bytes, 0, arr, 0, bytesRead);
+
+                    await _channel.Writer.WriteAsync(arr);
+                }
+            } while (bytesRead > 0);
+
+            _channel.Writer.Complete();
+        }
+
+        private async Task WriteTargetFile()
+        {
+            await using var target = new FileStream(_target, FileMode.Create, FileAccess.Write);
+
+            await foreach (var bytes in _channel.Reader.ReadAllAsync())
+            {
+                await target.WriteAsync(bytes);
+            }
+        }
+    }
+}
--- GZipChannels/Program.cs
+++ GZipChannels/Program.cs
@@ -1,77 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 using CommandLine;
 
 namespace GZipChannels
 {
-
-    internal class Archiver
-    {
-        private readonly string _source;
-        private readonly string _target;
-        private readonly Channel<byte[]> _channel;
-        private static int BlockSize = 10 * 1024 * 1024;
-        private static int ChannelSize = 100;
-
-        public Archiver(string source, string target)
-        {
-            _source = source;
-            _target = target;
-            _channel = Channel.CreateBounded<byte[]>(ChannelSize);
-        }
-
-        public async Task Run()
-        {
-            var writer = Task.Run(Writer);
-
-            await using var source = new FileStream(_source, FileMode.Open, FileAccess.Read);
-
-            var bytes = new byte[BlockSize];
-            int readed;
-
-            do
-            {
-                readed = await source.ReadAsync(bytes, 0, BlockSize);
-
-                if (readed == 0)
-                {
-                    break;
-                }
-
-                if (readed == BlockSize)
-                {
-                    await _channel.Writer.WriteAsync(bytes);
-                }
-
-                if (readed < BlockSize)
-                {
-                    var arr = new byte[readed];
-                    Buffer.BlockCopy(bytes, 0, arr, 0, readed);
-
-                    await _channel.Writer.WriteAsync(arr);
-                }
-
-            } while (readed > 0);
-
-            _channel.Writer.Complete();
-
-            await writer;
-        }
-
-        private async Task Writer()
-        {
-            using var target = new FileStream(_target, FileMode.Create, FileAccess.Write);
-
-            await foreach (var bytes in _channel.Reader.ReadAllAsync())
-            {
-                await target.WriteAsync(bytes);
-            }
-        }
-    }
-
     internal static class Program
     {
         private static async Task<int> Main(string[] args)
@@ -106,7 +39,7 @@ namespace GZipChannels
 
         private static async Task WithParsedAsync(GZipChannelsOptions arg)
         {
-            var a = new Archiver(arg.SourcePath, arg.TargetPath);
+            var a = new Copier(arg.SourcePath, arg.TargetPath);
             await a.Run();
         }
     }
